921b2251e66423751da3e1171ee9b30ddae2617b
sai charan <saicharan.gandham@gmail.com>
2025-01-28 17:14:36 +0530

    deployement fix 2

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,6 @@ const nextConfig: NextConfig = {
   typescript:{
     ignoreBuildErrors: true,
   },
-  output: 'export',
-  images: {
-    unoptimized: true, // Disable Image Optimization API for static builds
-  },
 };
 
 export default nextConfig;
